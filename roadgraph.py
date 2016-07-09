91b29cb21209fb91bc7236140397ff1d653429bf
Pavlo <pdyban@gmail.com>
2016-07-09 23:11:04 +0200

    Added a method to traverse all paths in a graph with any number of segments

--- roadgraph.py
+++ roadgraph.py
@@ -30,7 +30,36 @@ class RoadGraph(defaultdict):
     def traverse(self):
         """
         Traverses the road graph and returns pairs of neighboring waypoints.
+
+        :rtype iterator
         """
         for node, following_nodes in self.items():
-            for fn in folloing_nodes:
+            for fn in following_nodes:
                 yield (node, fn)
+
+    def traverse_paths(self, node=None, length=1):
+        """
+        Traverses the graph for paths of the required length.
+
+        :param node: starting node; if None, then all graph nodes are analyzed once
+        :param length: Length of each path segment.
+        :rtype iterator
+        """
+        # print('Node: ', node)
+        if node is None:
+            for node in self:
+                for path in self.traverse_paths(node, length):
+                    yield path
+
+        elif node not in self:
+            return  # node has no children
+
+        else:
+            if length == 1:
+                for child in self[node]:
+                    yield [node, child]
+
+            else:
+                for child in self[node]:
+                    for path in self.traverse_paths(child, length-1):
+                        yield [node] + path
--- test.py
+++ test.py
@@ -2,14 +2,88 @@ import unittest
 from roadcollector import *
 from roadclassifier import *
 from routebuilder import *
+from roadgraph import *
+
+
+class TestRoadGraph(unittest.TestCase):
+    def setUp(self):
+        # init an example graph
+        self.graph = RoadGraph()
+        self.graph.append_way([(0, 0), (0, 1), (0, 2), (0, 3)])
+        self.graph.append_way([(0, 0), (1, 1), (1, 2)])
+        self.graph.append_way([(1, 1), (2, 2)])
+        self.graph.append_way([(0, 0), (2, 1)])
+        self.graph.append_way([(1, 1), (3, 2), (3, 3)])
+        self.graph.append_way([(3, 2), (4, 3)])
+
+    def test_traverse_two_segment_paths_from_source(self):
+        """
+        Traverse all paths that contain exactly two segments, starting from a given source node.
+        """
+        ipaths = self.graph.traverse_paths(node=(0, 0), length=2)
+        paths = [path for path in ipaths]
+
+        self.assertIn([(0, 0), (0, 1), (0, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (1, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (3, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (2, 2)], paths)
+        self.assertEqual(len(paths), 4)
+
+    def test_traverse_all_two_segment_paths(self):
+        """
+        Traverse all paths in a graph that contain two segments.
+        """
+        ipaths = self.graph.traverse_paths(length=2)
+        paths = [path for path in ipaths]
+
+        self.assertIn([(0, 1), (0, 2), (0, 3)], paths)
+        self.assertIn([(0, 0), (0, 1), (0, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (1, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (3, 2)], paths)
+        self.assertIn([(0, 0), (1, 1), (2, 2)], paths)
+        self.assertIn([(1, 1), (3, 2), (3, 3)], paths)
+        self.assertIn([(1, 1), (3, 2), (4, 3)], paths)
+        self.assertEqual(len(paths), 7)
+
+    def test_traverse_all_three_segment_paths(self):
+        """
+        Traverse all paths in a graph that contain three segments.
+        """
+        ipaths = self.graph.traverse_paths(length=3)
+        paths = [path for path in ipaths]  # convert iterator to list
+
+        self.assertIn([(0, 0), (0, 1), (0, 2), (0, 3)], paths)
+        self.assertIn([(0, 0), (1, 1), (3, 2), (3, 3)], paths)
+        self.assertIn([(0, 0), (1, 1), (3, 2), (4, 3)], paths)
+        self.assertEqual(len(paths), 3)
+
+    def test_traverse_all_one_segment_paths(self):
+        """
+        Traverse all paths that contain one segments, i.e. all paths in a graph that connect two waypoints.
+        """
+        ipaths = self.graph.traverse_paths()
+        paths = [path for path in ipaths]  # convert iterator to list
+
+        self.assertIn([(0, 1), (0, 2)], paths)
+        self.assertIn([(3, 2), (3, 3)], paths)
+        self.assertIn([(3, 2), (4, 3)], paths)
+        self.assertIn([(0, 0), (0, 1)], paths)
+        self.assertIn([(0, 0), (1, 1)], paths)
+        self.assertIn([(0, 0), (2, 1)], paths)
+        self.assertIn([(0, 2), (0, 3)], paths)
+        self.assertIn([(1, 1), (1, 2)], paths)
+        self.assertIn([(1, 1), (3, 2)], paths)
+        self.assertIn([(1, 1), (2, 2)], paths)
+        self.assertEqual(len(paths), 10)
 
 
 class TestRoadCollectors(unittest.TestCase):
     def test_query_road_by_id(self):
         query = """way(47081339);out;"""
         roads = QueryRoadCollector().get_roads(query)
-        self.assertEqual(len(roads), 1)
-        self.assertEqual(len(roads[0]), 57)
+        print(roads)
+        self.assertEqual(len(roads), 56)
+        self.assertEqual(len(roads[0]), 1)
 
     def test_query_two_roads(self):
         query = """(way(161468888);way(47081339););
@@ -24,10 +98,10 @@ class TestRoadCollectors(unittest.TestCase):
 class TestRoadClassifiers(unittest.TestCase):
     def setUp(self):
         query = """way(47081339);out;"""
-        self.nodes = QueryRoadCollector().get_roads(query)
+        self.road_graph = QueryRoadCollector().get_roads(query)
 
     def test_vector_angle_classifier(self):
-        graph = VectorAngleRoadClassifier().get_classification(self.nodes)
+        graph = VectorAngleRoadClassifier().get_classification(self.road_graph)
         self.assertEqual(len(graph), 56)  # consists of list of road points aka nodes
         self.assertIsInstance(graph, dict)
         # for node in graph:
