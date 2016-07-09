638b503417ec7f9b8848de20bb66d7a326049e05
Pavlo <pdyban@gmail.com>
2016-07-09 23:24:46 +0200

    Refactored RoadClassifier to work on road graphs

--- roadclassifier.py
+++ roadclassifier.py
@@ -23,16 +23,15 @@ class VectorAngleRoadClassifier(RoadClassifier):
     def __init__(self):
         super().__init__()
 
-    def get_classification(self, nodes):
+    def get_classification(self, road_graph):
         """not finished yet"""
 
         def calculate_angle(p0, p1, p2):
             """
             Calculates angle between (p0,p1) and (p0,p2)
             """
-            import numpy as np
-            a = np.array((p0[0]-p1[0],p0[1]-p1[1]))
-            b = np.array((p0[0]-p2[0],p0[1]-p2[1]))
+            a = np.array((p0[0]-p1[0], p0[1]-p1[1]))
+            b = np.array((p0[0]-p2[0], p0[1]-p2[1]))
             return np.arccos(np.dot(a, b)/np.linalg.norm(a)/np.linalg.norm(b))
 
         def convert_to_meters(p0, p1):
@@ -57,50 +56,15 @@ class VectorAngleRoadClassifier(RoadClassifier):
             d = R * c
             return d
 
-        points = []
-        # fun_classes = []
-        g = {}  # graph
-        for path in nodes:
-            intensity = [0]*len(path)
-            total_spass = 0.0
-            total_distance = 0.0
-            for index in range(len(path)-2):
-                p0 = path[index]
-                p1 = path[index+1]
-                p2 = path[index+2]
-                p0_float = np.array(p0, dtype=np.float)
-                p1_float = np.array(p1, dtype=np.float)
-                p2_float = np.array(p2, dtype=np.float)
-                fun_factor = calculate_angle(p0_float, p1_float, p2_float)
-                intensity[index+1] = fun_factor
-                points.append([p1, fun_factor])
-                total_spass += fun_factor
-                if p0 not in g:
-                    g[p0] = {}
-                g[p0][p1] = fun_factor
+        for path in road_graph.traverse_paths(length=2):
+            p0, p1, p2 = path
+            p0_float = np.array(p0, dtype=np.float)
+            p1_float = np.array(p1, dtype=np.float)
+            p2_float = np.array(p2, dtype=np.float)
+            fun_factor = calculate_angle(p0_float, p1_float, p2_float)
+            road_graph[p0][p1] = fun_factor
 
-            # close the path with the last two points
-            if len(path) > 2:
-                p0 = path[-2]
-                p1 = path[-1]
-                if p0 not in g:
-                    g[p0] = {}
-
-                g[p0][p1] = 0.0
-
-            # if total_spass > 10:
-            #     c = 'black'
-            # elif total_spass > 6:
-            #     c = 'red'
-            # elif total_spass > 3:
-            #     c = 'green'
-            # elif total_spass > 1:
-            #     c = 'blue'
-            # else:
-            #     c = 'gray'
-            # fun_classes.append(c)
-
-        return g
+        return road_graph
 
 
 class TriangleRoadClassifier(RoadClassifier):
--- test.py
+++ test.py
@@ -81,18 +81,14 @@ class TestRoadCollectors(unittest.TestCase):
     def test_query_road_by_id(self):
         query = """way(47081339);out;"""
         roads = QueryRoadCollector().get_roads(query)
-        print(roads)
         self.assertEqual(len(roads), 56)
-        self.assertEqual(len(roads[0]), 1)
 
     def test_query_two_roads(self):
         query = """(way(161468888);way(47081339););
                 (._;>;);
                 out;"""
         roads = QueryRoadCollector().get_roads(query)
-        self.assertEqual(len(roads), 2)
-        self.assertEqual(len(roads[0]), 57)
-        self.assertEqual(len(roads[1]), 100)
+        self.assertEqual(len(roads), 155)
 
 
 class TestRoadClassifiers(unittest.TestCase):
