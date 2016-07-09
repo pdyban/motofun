9fd97a95391c7cf627a4f73eb8d941a21b4a4728
Pavlo <pdyban@gmail.com>
2016-07-09 23:37:13 +0200

    Removed plotting convenience function due to redundancy, new traverse function offers same functionality

--- roadgraph.py
+++ roadgraph.py
@@ -18,14 +18,14 @@ class RoadGraph(defaultdict):
         for prev_node, cur_node in zip(nodes[:-1], nodes[1:]):
             self[prev_node][cur_node] = 0.0
 
-    def get_waypoints_for_plotting(self):
-        """ converts road graph to x and y arrays, a convenience function for plotting for matplotlib """
-        x = []
-        y = []
-        for node, nodes in self.items():
-            x.append([node[0]] + [n[0] for n in nodes])
-            y.append([node[1]] + [n[1] for n in nodes])
-        return x, y
+    # def get_waypoints_for_plotting(self):
+    #     """ converts road graph to x and y arrays, a convenience function for plotting for matplotlib """
+    #     x = []
+    #     y = []
+    #     for node, nodes in self.items():
+    #         x.append([node[0]] + [n[0] for n in nodes])
+    #         y.append([node[1]] + [n[1] for n in nodes])
+    #     return x, y
 
     def traverse(self):
         """
