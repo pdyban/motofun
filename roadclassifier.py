7bae205cb3a2412660912cae9a003f69f38f4695
Pavlo <pdyban@gmail.com>
2016-07-13 00:41:48 +0200

    Modified angle feature

--- roadclassifier.py
+++ roadclassifier.py
@@ -1,6 +1,7 @@
 __author__ = 'missoni'
 
 import numpy as np
+from math import pi
 
 
 class RoadClassifier(object):
@@ -34,34 +35,64 @@ class VectorAngleRoadClassifier(RoadClassifier):
             b = np.array((p0[0]-p2[0], p0[1]-p2[1]))
             return np.arccos(np.dot(a, b)/np.linalg.norm(a)/np.linalg.norm(b))
 
+        def calculate_angle_per_distance(p0, p1, p2):
+            """
+            Calculates angle per distance feature.
+
+            Distance between p0 and p1, because the feature will be attached to that edge.
+            """
+            feature = calculate_angle(p0, p1, p2)
+            a = np.array((p0[0]-p1[0], p0[1]-p1[1]))
+            feature /= np.linalg.norm(a)
+            return feature
+
+        def to_radians(angle):
+            return angle*pi/180.0
+
+        def to_degrees(angle):
+            return angle*180.0/pi
+
         def convert_to_meters(p0, p1):
             """
             Converts distance between 2 2D points in geo coordinates to meters.
             """
-            def to_radians(angle):
-                from math import pi
-                return angle*pi/180.0
-
             from math import sin, cos, sqrt, atan2
             lat1, lon1 = p0
             lat2, lon2 = p1
             R = 6371 # km
+
+            lon1 = np.float(lon1)
+            lon2 = np.float(lon2)
+            lat1 = np.float(lat1)
+            lat2 = np.float(lat2)
+
             dLat = to_radians(lat2-lat1)
             dLon = to_radians(lon2-lon1)
             lat1 = to_radians(lat1)
             lat2 = to_radians(lat2)
 
-            a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2)
+            a = sin(dLat/2)**2 + sin(dLon/2)**2 * cos(lat1) * cos(lat2)
             c = 2 * atan2(sqrt(a), sqrt(1-a))
             d = R * c
-            return d
+            return d * 1000
 
         for path in road_graph.traverse_paths(length=2):
             p0, p1, p2 = path
             p0_float = np.array(p0, dtype=np.float)
             p1_float = np.array(p1, dtype=np.float)
             p2_float = np.array(p2, dtype=np.float)
-            fun_factor = calculate_angle(p0_float, p1_float, p2_float)
+            angle = calculate_angle(p0_float, p1_float, p2_float)
+            if to_degrees(angle) < 5:
+                angle = 0
+            elif to_degrees(angle) > 40:
+                angle = 0
+            # if to_degrees(angle) > 40 or to_degrees(angle) < 10:
+            #     fun_factor = 0
+            # else:
+            #     fun_factor = 1
+
+            fun_factor = to_degrees(angle)
+
             road_graph[p0][p1] = fun_factor
 
         return road_graph
