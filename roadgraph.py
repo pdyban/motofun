f8b7a58dc030c4b688a71756935ea95f9351cb78
Pavlo <pdyban@gmail.com>
2016-07-13 00:45:19 +0200

    Implemented graph simplification algorithm

--- roadgraph.py
+++ roadgraph.py
@@ -1,12 +1,15 @@
 from collections import defaultdict
+from copy import deepcopy
 
 
 class RoadGraph(defaultdict):
     """
-    Graph implementation for easy access
+    Graph implementation for easy access.
+
+    :param memo: parameter for deepcopy operator.
     """
-    def __init__(self):
-        defaultdict.__init__(self, dict)
+    def __init__(self, memo=None):
+        super().__init__(dict)
 
     def append_way(self, nodes):
         """
@@ -17,15 +20,9 @@ class RoadGraph(defaultdict):
         """
         for prev_node, cur_node in zip(nodes[:-1], nodes[1:]):
             self[prev_node][cur_node] = 0.0
-
-    # def get_waypoints_for_plotting(self):
-    #     """ converts road graph to x and y arrays, a convenience function for plotting for matplotlib """
-    #     x = []
-    #     y = []
-    #     for node, nodes in self.items():
-    #         x.append([node[0]] + [n[0] for n in nodes])
-    #         y.append([node[1]] + [n[1] for n in nodes])
-    #     return x, y
+            self[cur_node][prev_node] = 0.0  # double-sided directed graph
+        # if nodes[-1] not in self:
+        #    self[nodes[-1]] = {}
 
     def traverse(self):
         """
@@ -62,4 +59,82 @@ class RoadGraph(defaultdict):
             else:
                 for child in self[node]:
                     for path in self.traverse_paths(child, length-1):
-                        yield [node] + path
+                        if node not in path:
+                            yield [node] + path
+
+    # mirror is not useful for double-directed graphs
+    def mirror(self):
+        """
+        Generates a mirrored graph, i.e. a graph (with the same number of nodes)
+        whose edges are directed in the inverse direction to the given graph.
+
+        :return: the mirrored graph
+        :rtype: RoadGraph
+        """
+        mirrored_graph = RoadGraph()
+        for path in self.traverse_paths(length=1):
+            start, finish = path
+            mirrored_graph[finish][start] = self[start][finish]
+            if start not in mirrored_graph:
+                mirrored_graph[start] = {}
+        return mirrored_graph
+
+    def find_next_node(self, prev_node, cur_node):
+        """
+        Finds next final node with more than 1 child. Required for graph simplification algorithm.
+
+        :param prev_node: previous node, limits the search from running backwards
+        :param cur_node: current node
+        :return: (total_length, new_end_node, path between cur_node and new_end_node that can be simplified)
+        """
+        if prev_node not in self:
+            raise AttributeError("Previous node not found!" + str(prev_node))
+        if cur_node not in self:
+            raise AttributeError("Current node not found!" + str(cur_node))
+
+        total_length = self[prev_node][cur_node]
+        children = [ch for ch in self[cur_node] if ch != prev_node]
+        path = []
+        while len(children) == 1:
+            if children[0] in path:
+                break  # cycle!
+            path.append(cur_node)
+            prev_node = cur_node
+            cur_node = children[0]
+            children = [ch for ch in self[cur_node] if ch != prev_node]
+            total_length += self[prev_node][cur_node]
+
+        return total_length, cur_node, path
+
+    def simplify(self, copy=True):
+        """
+        Simplifies graph, i.e. removes all nodes that contain only one child
+        by connecting the preceding node to the following.
+
+        :param copy: should create a copy of the graph; if False, will overwrite current graph
+        :return: the simplified graph, where each node has more than 1 child, or zero (if end node).
+        :rtype: RoadGraph
+        """
+        if copy:
+            simplified_graph = deepcopy(self)
+        else:
+            simplified_graph = self
+
+        for path in self.traverse_paths(length=1):
+            start, finish = path
+
+            if start not in simplified_graph or finish not in simplified_graph:
+                continue  # already simplified
+
+            total_length, next_node, path_to_remove = simplified_graph.find_next_node(start, finish)
+            simplified_graph[start][next_node] = total_length
+            simplified_graph[next_node][start] = total_length
+            for node in path_to_remove:  # for each node in the simplification path, remove:
+                if node in simplified_graph:  # if not yet removed
+                    del simplified_graph[node]  # remove
+
+            if path_to_remove:
+                del simplified_graph[start][path_to_remove[0]]
+                del simplified_graph[next_node][path_to_remove[-1]]
+
+        return simplified_graph
--- test.py
+++ test.py
@@ -43,7 +43,7 @@ class TestRoadGraph(unittest.TestCase):
         self.assertIn([(0, 0), (1, 1), (2, 2)], paths)
         self.assertIn([(1, 1), (3, 2), (3, 3)], paths)
         self.assertIn([(1, 1), (3, 2), (4, 3)], paths)
-        self.assertEqual(len(paths), 7)
+        self.assertEqual(len(paths), 28)
 
     def test_traverse_all_three_segment_paths(self):
         """
@@ -55,7 +55,7 @@ class TestRoadGraph(unittest.TestCase):
         self.assertIn([(0, 0), (0, 1), (0, 2), (0, 3)], paths)
         self.assertIn([(0, 0), (1, 1), (3, 2), (3, 3)], paths)
         self.assertIn([(0, 0), (1, 1), (3, 2), (4, 3)], paths)
-        self.assertEqual(len(paths), 3)
+        self.assertEqual(len(paths), 30)
 
     def test_traverse_all_one_segment_paths(self):
         """
@@ -74,21 +74,80 @@ class TestRoadGraph(unittest.TestCase):
         self.assertIn([(1, 1), (1, 2)], paths)
         self.assertIn([(1, 1), (3, 2)], paths)
         self.assertIn([(1, 1), (2, 2)], paths)
-        self.assertEqual(len(paths), 10)
+        self.assertEqual(len(paths), 2*10)  # double the number because the graph is double-sided directed
+
+    # mirror is not useful for double-directed graphs
+    @unittest.skip
+    def test_mirror(self):
+        length = len(self.graph)
+        mirrored_graph = self.graph.mirror()
+        self.assertEqual(len(self.graph), length)  # the original graph has not been modified
+        self.assertEqual(len(mirrored_graph), len(self.graph))  # the mirrored graph has the same number of nodes
+        for node in mirrored_graph:
+            self.assertIn(node, self.graph)  # each node from the mirrored graph is in the original graph
+            # cardinality of the mirrored node should be equal to the total number of nodes
+            # that reference that node in the original graph
+            self.assertEqual(len(mirrored_graph[node]),
+                             sum(1 for n in self.graph if n in self.graph and node in self.graph[n]))
+
+    def test_find_next_node(self):
+        length, last_node, path = self.graph.find_next_node((0, 0), (0, 1))
+        self.assertEqual(last_node, (0, 3))
+        self.assertIn((0, 2), path)
+        self.assertIn((0, 1), path)
+
+        length, last_node, path = self.graph.find_next_node((0, 3), (0, 2))
+        self.assertEqual(last_node, (0, 0))
+        self.assertIn((0, 2), path)
+        self.assertIn((0, 1), path)
+        self.assertNotIn((0, 0), path)
+
+        length, last_node, path = self.graph.find_next_node((0, 0), (1, 1))
+        self.assertEqual(last_node, (1, 1))
+        self.assertEqual(len(path), 0)
+
+    def test_simplify(self):
+        simplified_graph = self.graph.simplify()
+        simplified_graph = simplified_graph.simplify()
+        self.assertEqual(len(simplified_graph), 9)
+
+    def test_simplify_one_segment_graph(self):
+        graph = RoadGraph()
+        graph.append_way([(0, 0), (1, 1)])
+        simplified_graph = graph.simplify()
+        self.assertEqual(len(simplified_graph), 2)  # should not remove any nodes
+
+    def test_simplify_cycled_graph(self):
+        graph = RoadGraph()
+        graph.append_way([(0, 0), (1, 1), (2, 2)])
+        graph.append_way([(0, 0), (0, 1), (2, 2)])
+        simplified_graph = graph.simplify()
+        self.assertEqual(len(simplified_graph), 1)  # should keep one node with cycle
+
+        graph.append_way([(2, 2), (3, 3)])
+        simplified_graph = graph.simplify()
+        self.assertEqual(len(simplified_graph), 2)  # should simplify graph to one segment
+
+    def test_simplify_raph_with_multiple_directions(self):
+        graph = RoadGraph()
+        graph.append_way([(0, 0), (1, 1), (2, 2), (3, 3)])
+        graph.append_way([(4, 4), (3, 3)])
+        simplified_graph = graph.simplify()
+        self.assertEqual(len(simplified_graph), 2)  # should keep one segment
 
 
 class TestRoadCollectors(unittest.TestCase):
     def test_query_road_by_id(self):
         query = """way(47081339);out;"""
         roads = QueryRoadCollector().get_roads(query)
-        self.assertEqual(len(roads), 56)
+        self.assertEqual(len(roads), 57)
 
     def test_query_two_roads(self):
         query = """(way(161468888);way(47081339););
                 (._;>;);
                 out;"""
         roads = QueryRoadCollector().get_roads(query)
-        self.assertEqual(len(roads), 155)
+        self.assertEqual(len(roads), 156)
 
 
 class TestRoadClassifiers(unittest.TestCase):
@@ -98,7 +157,7 @@ class TestRoadClassifiers(unittest.TestCase):
 
     def test_vector_angle_classifier(self):
         graph = VectorAngleRoadClassifier().get_classification(self.road_graph)
-        self.assertEqual(len(graph), 56)  # consists of list of road points aka nodes
+        self.assertEqual(len(graph), 57)  # consists of list of road points aka nodes
         self.assertIsInstance(graph, dict)
         # for node in graph:
         #     for other_node in graph[node]:
