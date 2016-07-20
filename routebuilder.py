eea65cd0b4fb77b7028302d257dee7e0916378d1
Pavlo <pdyban@gmail.com>
2016-07-20 11:44:01 +0200

    Added shortest path route builder

--- routebuilder.py
+++ routebuilder.py
@@ -1,4 +1,5 @@
-__author__ = 'missoni'
+import heapq
+from math import log
 
 
 class RouteBuilder(object):
@@ -6,6 +7,57 @@ class RouteBuilder(object):
     def __init__(self):
         super().__init__()
 
-    def get_route(self, *args, **kwargs):
-        """re-consider function signature here"""
+    def build(self, graph, *args, **kwargs):
+        """
+        Constructs a route in the given graph provided the options.
+
+        :param graph: the graph that represents the road system
+        :type graph: RoadGraph
+        :return: list of waypoints that constitute the route
+        """
         raise NotImplemented("Implement me in children!")
+
+
+class ShortestPathRouteBuilder(RouteBuilder):
+    """
+    Constructs the shortest path in the road graph.
+    """
+    def build(self, graph, source, target):
+        """
+        Constructs the shortest path in the given graph
+        that runs from the source to the target node.
+
+        :param graph: the graph that represents the road system
+        :type graph: RoadGraph
+        :return: list of waypoints that constitute the shortest path
+        """
+        return self.shortestPath(graph, source, target)
+
+    def shortestPath(self, G, start, end):
+        """
+        Dijkstra's algorithm for shortest paths
+        David Eppstein, UC Irvine, 4 April 2002
+        http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/117228
+
+        :param G: the graph in dict(dict) format
+        :param start: the starting node aka the source
+        :param end: the finish node aka the sink
+        :return: list of waypoints that constitute the shortest path
+        """
+        def flatten(L):       # Flatten linked list of form [0,[1,[2,[]]]]
+            while len(L) > 0:
+                yield L[0]
+                L = L[1]
+
+        q = [(0, start, ())]  # Heap of (cost, path_head, path_rest).
+        visited = set()       # Visited vertices.
+        while True:
+            (cost, v1, path) = heapq.heappop(q)
+            if v1 not in visited:
+                visited.add(v1)
+                if v1 == end:
+                    return list(flatten(path))[::-1] + [v1]
+            path = (v1, path)
+            for (v2, cost2) in G[v1].items():
+                if v2 not in visited:
+                    heapq.heappush(q, (cost + cost2, v2, path))
--- test/testroutebuilders.py
+++ test/testroutebuilders.py
@@ -1,17 +1,37 @@
 import unittest
+from roadcollector import RoadCollector
 from roadclassifier import VectorAngleRoadClassifier
-from roadcollector import QueryRoadCollector
-from routebuilder import RouteBuilder
-
-__author__ = 'missoni'
+from routebuilder import ShortestPathRouteBuilder
+from roadgraph import RoadGraph
 
 
-@unittest.skip
 class TestRouteBuilders(unittest.TestCase):
     def setUp(self):
-        query = """way(47081339);out;"""
-        self.nodes = QueryRoadCollector().get_roads(query)
-        self.classes = VectorAngleRoadClassifier().get_classification(self.nodes)
+        # init an example graph
+        self.graph = RoadGraph()
+        self.graph.append_way([(0, 0), (0, 1), (0, 2), (0, 3)])
+        self.graph.append_way([(0, 0), (1, 1), (1, 2)])
+        self.graph.append_way([(1, 1), (2, 2)])
+        self.graph.append_way([(0, 0), (2, 1)])
+        self.graph.append_way([(1, 1), (3, 2), (3, 3)])
+        self.graph.append_way([(3, 2), (4, 3)])
+
+        classifier = VectorAngleRoadClassifier()
+        self.classified_graph = classifier.get_classification(self.graph)
+
+        # query = """way(47081339);out;"""
+        # self.nodes = QueryRoadCollector().get_roads(query)
+        # self.classes = VectorAngleRoadClassifier().get_classification(self.nodes)
 
     def test_build_shortest_route(self):
-        rb = RouteBuilder()
+        rb = ShortestPathRouteBuilder()
+        path = rb.build(self.classified_graph, (0, 0), (3, 3))
+        for waypoint in [(0, 0), (1, 1), (3, 2), (3, 3)]:
+            self.assertIn(waypoint, path, "Waypoint {} is not in the shortest path".format(waypoint))
+
+    def test_shortest_path_is_connected(self):
+        rb = ShortestPathRouteBuilder()
+        path = rb.build(self.classified_graph, (0, 0), (3, 3))
+        for segment in zip(path[:-1], path[1:]):
+            self.assertTrue(list(segment) in self.graph.edges(),
+                            "Segment {} is not available in the RoadGraph".format(str(segment)))
